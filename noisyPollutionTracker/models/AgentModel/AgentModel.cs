04b84e427e8b8892e9acac83405a3a829b2c614f
Mirjalol Ziyadullayev <121923945+mirjalolziyadullayev@users.noreply.github.com>
2024-01-29 19:18:52 +0500

    Add files via upload

---
+++ noisyPollutionTracker/models/AgentModel/AgentModel.cs
@@ -0,0 +1,11 @@
+﻿using noisyPollutionTracker.models.CityModels;
+
+namespace noisyPollutionTracker.models.AgentModel;
+
+public class AgentModel
+{
+    public int Id { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public List<CityModel> DiscoveredCities { get; set; }
+}
---
+++ noisyPollutionTracker/models/AgentModel/AgentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace noisyPollutionTracker.models.AgentModel;
+
+public class AgentViewModel
+{
+    public int Id { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+}
